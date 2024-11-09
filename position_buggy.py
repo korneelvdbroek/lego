faac1f5701189e79294799f46097e526a700c71e
korneel <korneelvdbroek@gmail.com>
2024-11-09 14:42:15 +0100

    * working version of position buggy!!

--- position_buggy.py
+++ position_buggy.py
@@ -12,7 +12,7 @@ import urandom
 class Buggy:
     # buggy characteristics
     cm_for_1000_degrees = 153. / 1106. * 1000  # distance in cm when drive-engine rotates 1000 degrees
-    steer_angle_max = 15  # max angle of front wheels
+    steer_angle_max_deg = 15  # max angle of front wheels
     buggy_length_cm = 15.5  # distance between front and back axel
 
     def __init__(self):
@@ -44,7 +44,7 @@ class Buggy:
         #     raise e
         # print(f"Remote control initialized...")
 
-        self.engine_steer_angle_max = self.calibrate_steering()
+        self.engine_steer_angle_max_deg = self.calibrate_steering()
         print(f"Steering initialized...")
 
         # Lower the acceleration so the car starts and stops realistically.
@@ -69,12 +69,12 @@ class Buggy:
         # We are now at the right. Reset this angle to be half the difference.
         # That puts zero in the middle.
         self._engine_steer.reset_angle(right_end - (right_end + left_end) / 2)
-        engine_steer_angle_max = 0.9 * self._engine_steer.angle()
+        engine_steer_angle_max_deg = 0.9 * self._engine_steer.angle()
 
         # reset steer to neutral position
         self._engine_steer.run_target(speed=200, target_angle=0, wait=True)
 
-        return engine_steer_angle_max
+        return engine_steer_angle_max_deg
 
     async def track_task(self):
         self._program_stop = False
@@ -88,15 +88,17 @@ class Buggy:
             distance_delta = self.drive_angle_to_cm(drive_angle_delta)
 
             # angle
-            self.direction_angle = self.hub.imu.heading()
+            self.direction_angle = self.hub.imu.heading() / 180 * umath.pi
 
             # position
-            self.x += distance_delta * umath.cos(self.direction_angle / 180 * umath.pi)
-            self.y += distance_delta * umath.sin(self.direction_angle / 180 * umath.pi)
+            self.x += distance_delta * umath.cos(self.direction_angle)
+            self.y += distance_delta * umath.sin(self.direction_angle)
 
-            print(f"(x, y) = ({self.x}, {self.y})   (angle = {self.direction_angle})")
+            print(f"  (x, y)     = ({self.x:5.1f}, {self.y:5.1f})   (direction_angle = {self.direction_angle * 180 / umath.pi:3.0f})")
             # crash detection
-            # print(f"  (a_x, a_y) = ({self.hub.imu.acceleration(Axis.X)}, {self.hub.imu.acceleration(Axis.Y)})")
+            if self._engine_drive.stalled():
+                print(f"crash!")
+            print(f"  (a_x, a_y) = ({self.hub.imu.acceleration(Axis.X):5.1f}, {self.hub.imu.acceleration(Axis.Y):5.1f})")
             await wait(10)
 
             #
@@ -117,25 +119,41 @@ class Buggy:
         await self.drive_angle(speed, self.cm_to_drive_angle(distance_cm))
 
     async def drive_xy(self, x_target, y_target, speed=500):
-        if x_target >= self.x:
-            self._engine_drive.run(speed)
-        else:
-            # drive backwards
-            self._engine_drive.run(-speed)
         self._engine_drive.run(speed)
 
         accepted_error_cm = 20
+        drive_forward_old = True
         while (x_target - self.x) ** 2 + (y_target - self.y) ** 2 > accepted_error_cm**2:
             # see backup computation
-            steer_angle = 180 / umath.pi * umath.atan2(self.buggy_length_cm * (y_target - self.y),
-                                                       2 * ((x_target - self.x) ** 2 + (y_target - self.y) ** 2))
+            steer_angle = umath.atan2(2 * self.buggy_length_cm * ((y_target - self.y) * umath.cos(self.direction_angle) - (x_target - self.x) * umath.sin(self.direction_angle)),
+                                      (x_target - self.x) ** 2 + (y_target - self.y) ** 2)
 
-            steer_angle = min(max(steer_angle, -self.steer_angle_max), self.steer_angle_max)
-            engine_steer_angle = steer_angle / self.steer_angle_max * self.engine_steer_angle_max
-            # print(f"steer_angle = {steer_angle} (engine_steer_angle = {engine_steer_angle})")
+            drive_forward = (x_target - self.x) * umath.cos(self.direction_angle) + (y_target - self.y) * umath.sin(self.direction_angle) >= 0
 
-            self._engine_steer.run_target(speed=500, target_angle=engine_steer_angle, wait=False)
+            # print(f"steering:")
+            # print(f"  drive_forward = {drive_forward}")
+            # print(f"  direction_angle = {self.direction_angle * 180 / umath.pi}")
+            # print(f"  (y_target - self.y) = {(y_target - self.y)}")
+            # print(f"  cos(direction_angle) = {umath.cos(self.direction_angle)}")
+            # print(f"  (x_target - self.x) = {(x_target - self.x)}")
+            # print(f"  sin(direction_angle) = {umath.sin(self.direction_angle)}")
+            # print(f"  {steer_angle} = atan({2 * self.buggy_length_cm * ((y_target - self.y) * umath.cos(self.direction_angle) - (x_target - self.x) * umath.sin(self.direction_angle))} / {(x_target - self.x) ** 2 + (y_target - self.y) ** 2})")
+
+            steer_angle_deg = 180 / umath.pi * steer_angle
+
+            steer_angle_deg = min(max(steer_angle_deg, -self.steer_angle_max_deg), self.steer_angle_max_deg)
+            engine_steer_angle_deg = steer_angle_deg / self.steer_angle_max_deg * self.engine_steer_angle_max_deg
+            # print(f"  steer_angle_deg = {steer_angle_deg:3.0f} (engine_steer_angle_deg = {engine_steer_angle_deg:3.0f})")
+
+            self._engine_steer.run_target(speed=500, target_angle=engine_steer_angle_deg, wait=False)
+            if drive_forward != drive_forward_old:
+                if drive_forward:
+                    self._engine_drive.run(speed)
+                else:
+                    self._engine_drive.run(-speed)
             await wait(20)
+            drive_forward_old = drive_forward
+
         self._engine_drive.stop()
         self._engine_steer.stop()
 
@@ -158,21 +176,22 @@ buggy = Buggy()
 async def buggy_program2():
     x_target = 0
     y_target = 0
-    no_go_radius = buggy.buggy_length_cm / umath.tan(buggy.steer_angle_max)
+    no_go_radius = buggy.buggy_length_cm / umath.tan(buggy.steer_angle_max_deg / 180 * umath.pi)
     for i in range(10):
 
+        # pick new position outside no-go zone
         while True:
-            x_target_delta = urandom.uniform(-50, 50)
-            y_target_delta = urandom.uniform(-50, 50)
+            x_target_delta = urandom.uniform(-150, 150)
+            y_target_delta = urandom.uniform(-150, 150)
             # check if it's in the no-go zone (turn is too sharp to get there...)
             if (x_target_delta)**2 + (y_target_delta - no_go_radius)**2 > 1.05 * no_go_radius and \
                (x_target_delta)**2 + (y_target_delta + no_go_radius)**2 > 1.05 * no_go_radius and \
-               -100 < x_target + x_target_delta < 100 and -100 < y_target + y_target_delta < 100:
+               -150 < x_target + x_target_delta < 150 and -150 < y_target + y_target_delta < 150:
                 break
 
         x_target += x_target_delta
         y_target += y_target_delta
-        speed = urandom.randint(0, 1000)
+        speed = urandom.randint(500, 1000)
         print(f"(x_target, y_target) = ({x_target}, {y_target})   speed = {speed}")
         await buggy.drive_xy(x_target, y_target, speed)
 
@@ -185,8 +204,7 @@ async def buggy_program():
     # await wait(1000)
     # await buggy.drive_distance(200, 100)
     # await buggy.drive_distance(200, 100)
-    await buggy.drive_xy(x_target=200, y_target=50)
-    # await wait(1000)
+    await buggy.drive_xy(x_target=200, y_target=130)    # await wait(1000)
 
 
 async def buggy_program_wrapper():
